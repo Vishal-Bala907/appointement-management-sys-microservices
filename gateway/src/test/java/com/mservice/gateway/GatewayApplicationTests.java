89c57688c378713bbce2439ee39efa7d2889d53c
vishal bala linux <vishal.bala.100@gmail.com>
2025-03-27 09:33:42 +0530

    initial commit, gateway added with User-service and appointment-service

---
+++ gateway/src/test/java/com/mservice/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mservice.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ user-service/src/main/java/com/mservice/users/controllers/TestController.java
@@ -0,0 +1,15 @@
+package com.mservice.users.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+	
+	@GetMapping("/user")
+	public String test() {
+		return "This is user service";
+	}
+}
